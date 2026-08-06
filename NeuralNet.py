a9fa1337689dc83ffec907e8a36ca9717566a3d0
Vishal Tyagi <visualtaggy@gmail.com>
2026-08-05 19:15:04 -0700

    fcnn

--- NeuralNet.py
+++ NeuralNet.py
@@ -30,8 +30,19 @@ class Layer:
         return out
 
 
+class FCNN:
+
+    def __init__(self, number_of_inputs, layer_struct):
+        total = [number_of_inputs] + layer_struct
+        self.layers = [Layer(total[i],total[i+1]) for i in range (len(layer_struct))]
+
+    def __call__(self,x):
+        for layer in self.layers:   
+            x = layer(x)
+        return x
+
 print("=============")
 
-x = [2.0, 3.0]
-n = Layer(2,4)
+x = [2.0, 3.0, 4.0]
+n = FCNN(3,[4,4,1])
 print(n(x))
